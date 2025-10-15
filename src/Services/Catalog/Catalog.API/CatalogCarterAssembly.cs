17495f2e98d8af7e2167fe5d284df19444d9f994
Manuel Rodríguez <Manuelol2000@gmail.com>
2025-10-15 00:38:44 -0700

    Session 6: Get By Categories

---
+++ src/Services/Catalog/Catalog.API/CatalogCarterAssembly.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace Catalog.API;
+
+public class CatalogCarterAssembly : DependencyContextAssemblyCatalog
+{
+    public override IReadOnlyCollection<Assembly> GetAssemblies()
+        => [typeof(Program).Assembly];
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductsByIdEndpoint.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Catalog.API.Products.GetProductById;
+
+//public record GetProductByIdRequest(Guid ProductId);
+
+public record GetProductByIdResponse(Product Product);
+
+public class GetProductsByIdEndpoint : ICarterModule
+{
+	public void AddRoutes(IEndpointRouteBuilder app)
+	{
+		app.MapGet("/products/{id:guid}", async (Guid id, ISender sender) =>
+		{
+			var query = new GetProductByIdQuery(id);
+			var result = await sender.Send(query);
+			var response = result.Adapt<GetProductByIdResponse>();
+			return Results.Ok(response);
+		})
+		.WithName("GetProductById")
+		.Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+		.ProducesProblem(StatusCodes.Status400BadRequest)
+		.ProducesProblem(StatusCodes.Status404NotFound)
+		.WithSummary("Gets a product by ID")
+		.WithDescription("Gets a product from the catalog by its ID");
+	}
+}
